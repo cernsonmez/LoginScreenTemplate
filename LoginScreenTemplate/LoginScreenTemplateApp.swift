fa62b79e6fe69b324c296ecb72e79833576de59f
Ceren Sonmez <ceren.sonmez@ibtech.com.tr>
2022-10-03 11:29:52 +0300

    Initial Commit

---
+++ LoginScreenTemplate/LoginScreenTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenTemplateApp.swift
+//  LoginScreenTemplate
+//
+//  Created by Ceren on 3.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
